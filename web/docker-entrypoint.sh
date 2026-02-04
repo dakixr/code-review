42f0377a71b675ee3c44fd1a1346bf9187a8f669
dakixr <dakixr@gmail.com>
2026-02-04 01:21:45 +0100

    Fix missing static output css

--- web/docker-entrypoint.sh
+++ web/docker-entrypoint.sh
@@ -9,22 +9,29 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 import django
 django.setup()
 from django.conf import settings
-from django.templatetags.static import static
 
 print("DJANGO_DEBUG =", settings.DEBUG)
 print("settings file =", settings.SETTINGS_MODULE, getattr(__import__('core.settings', fromlist=['__file__']), '__file__', 'unknown'))
 print("STATIC_URL =", settings.STATIC_URL)
 print("STATIC_ROOT =", settings.STATIC_ROOT)
 print("STATICFILES_DIRS =", getattr(settings, "STATICFILES_DIRS", None))
-try:
-    print("static('css/output.css') =", static("css/output.css"))
-except Exception as e:
-    print("static('css/output.css') failed:", repr(e))
 PY
 
   /opt/venv/bin/python manage.py migrate --noinput
   mkdir -p /app/static
   /opt/venv/bin/python manage.py collectstatic --noinput
+
+  /opt/venv/bin/python - <<'PY'
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+import django
+django.setup()
+from django.templatetags.static import static
+from django.conf import settings
+print("After collectstatic:")
+print("DJANGO_DEBUG =", settings.DEBUG)
+print("static('css/output.css') =", static("css/output.css"))
+PY
 fi
 
 exec "$@"
